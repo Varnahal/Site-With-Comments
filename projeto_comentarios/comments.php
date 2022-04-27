22e3b4ad24af22ca74e717bf733ac61cc44d260e
Daniel Marcelino de Lima <varnahal0712@gmail.com>
2022-04-27 15:19:38 -0300

    começo do front-end

---
+++ projeto_comentarios/comments.php
@@ -0,0 +1,61 @@
+<!DOCTYPE html>
+<html lang="pt-br">
+<head>
+    <meta charset="UTF-8">
+    <meta http-equiv="X-UA-Compatible" content="IE=edge">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <link rel="stylesheet" href="CSS/comments.css">
+    <title>Comentários</title>
+</head>
+<body>
+    <nav>
+        <ul>
+            <li><a href="index.php">Home</a></li>
+            <li><a href="">Entrar</a></li>
+        </ul>
+    </nav>
+    <div id="largura">
+    <h1>Guia Definitivo Como Criar um Blog Incrível e Ganhar Dinheiro Com Ele</h1>
+        <section id='conteudo1'>
+            
+            <img src="imagens/jose.jpg" alt="José">
+            <p class="text">É um fato há muito estabelecido que um leitor se distrairá com o conteúdo legível de uma página ao analisar seu layout. O ponto de usar o Lorem Ipsum é que ele tem uma distribuição de letras mais ou menos normal, em vez de usar 'Conteúdo aqui, conteúdo aqui'.</p>
+            <p class="text">1. O ponto de usar o Lorem Ipsum</p>
+            <p class="text">2. È que ele tem uma distribuição de letras</p>
+            <p class="text">3. Lorem Ipsum é que ele tem uma distribuição</p>
+            <p class="text">4. letras mais ou menos normal</p>
+            <h2>Deixe seu comentário</h2>
+            <form action="" method="post">
+                <img src="imagens/perfil.png" alt="imagemperfil">
+                <textarea name="text" id="text" cols="30" rows="10" placeholder="Digita algun bagui aí"></textarea>
+                <input type="submit" value="Publicar">
+            </form>
+            <div class="comment-area">
+                <img src="imagens/perfil.png" alt="">
+                <h3>Nome do ser vivo</h3>
+                <h4>hora e data <a href="">Excluir</a></h4>
+                <p>Lorem ipsum dolor sit amet consectetur adipisicing elit. Sequi quidem exercitationem dignissimos quaerat quibusdam, nihil deserunt. Ratione recusandae quas sapiente, repudiandae qui, eos nam optio enim reiciendis laboriosam id maiores?</p>
+            </div>
+            <div class="comment-area">
+                <img src="imagens/perfil.png" alt="">
+                <h3>Nome do ser vivo</h3>
+                <h4>hora e data <a href="">Excluir</a></h4>
+                <p>Lorem ipsum dolor sit amet consectetur adipisicing elit. Sequi quidem exercitationem dignissimos quaerat quibusdam, nihil deserunt. Ratione recusandae quas sapiente, repudiandae qui, eos nam optio enim reiciendis laboriosam id maiores?</p>
+            </div>
+            <div class="comment-area">
+                <img src="imagens/perfil.png" alt="">
+                <h3>Nome do ser vivo</h3>
+                <h4>hora e data <a href="">Excluir</a></h4>
+                <p>Lorem ipsum dolor sit amet consectetur adipisicing elit. Sequi quidem exercitationem dignissimos quaerat quibusdam, nihil deserunt. Ratione recusandae quas sapiente, repudiandae qui, eos nam optio enim reiciendis laboriosam id maiores?</p>
+            </div>
+            <div class="comment-area">
+                <img src="imagens/perfil.png" alt="">
+                <h3>Nome do ser vivo</h3>
+                <h4>hora e data <a href="">Excluir</a></h4>
+                <p>Lorem ipsum dolor sit amet consectetur adipisicing elit. Sequi quidem exercitationem dignissimos quaerat quibusdam, nihil deserunt. Ratione recusandae quas sapiente, repudiandae qui, eos nam optio enim reiciendis laboriosam id maiores?</p>
+            </div>
+        </section>
+        
+    </div>
+</body>
+</html>
