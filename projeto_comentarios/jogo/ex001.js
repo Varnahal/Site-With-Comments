5cbfd4723c02f1c26aebcc01798aafcea2ca6026
Daniel Marcelino de Lima <varnahal0712@gmail.com>
2022-06-23 20:20:24 -0300

    arrumei o contador

--- projeto_comentarios/jogo/ex001.js
+++ projeto_comentarios/jogo/ex001.js
@@ -20,44 +20,74 @@ function morreu(){
     var morreu = false;
     var cont = 0
     div1.style.display = 'none'
+    // verifica se o 'mario' morreu
     setInterval(() => {
         var coordsl = Number(cano.offsetLeft);
         var coordst = Number(div.offsetTop);
-        
         if(coordsl <= 173  && coordst > 556 && coordsl >26){
-            
-        div.style.display = 'none';
-        div1.style.display = 'flex';
-        div1.style.top = coordst+'px'
-        morreu = true
-        cano.style.animation = 'nada'
-        cano.style.display ='none'
-        butao.onclick = function() {arroz()};
+            div.style.display = 'none';
+            div1.style.display = 'flex';
+            div1.style.top = coordst+'px'
+            morreu = true
+            cano.style.animation = 'nada'
+            cano.style.display ='none'
+            butao.onclick = function() {arroz()};
         }
         
     }, 100); 
-
-    setInterval(function muda() {
+    // muda a velocidade do cano
+    const speed = setInterval(function muda() {
         if(cont > 4){
             body.classList.add('speed');
         }
-        if(cont > 19){
+        if(cont == 20){
             body.classList.remove('speed');
             body.classList.add('speed1');
         }
-        if(cont > 49){
+        if(cont == 50){
             body.classList.remove('speed1');
             body.classList.add('speed2');
+            clearInterval(speed);
         }    
     }, 10);
-    
-    setInterval(() => {
+    //ajusta a velocidade do contador com base no na valor do contador
+    const myInterval = setInterval(myTimer, 2000);
+    function myTimer(){
         if(morreu == false){
             cont ++  
             contador.innerHTML = cont;
         }
-    }, 2000);
-   
+        if(cont > 4){
+            clearInterval(myInterval);
+            const myInterval1 = setInterval(myTimer1, 1500);
+            function myTimer1(){
+                if(morreu == false){
+                    cont ++  
+                    contador.innerHTML = cont;
+                }
+                if(cont > 19){
+                    clearInterval(myInterval1);
+                    const myInterval2 = setInterval(myTimer2, 1200);
+                    function myTimer2(){
+                        if(morreu == false){
+                            cont ++  
+                            contador.innerHTML = cont;
+                        }
+                        if(cont > 49){
+                            clearInterval(myInterval2);
+                            const myInterval3 = setInterval(myTimer3, 1000);
+                            function myTimer3(){
+                                if(morreu == false){
+                                    cont ++  
+                                    contador.innerHTML = cont;
+                                }
+                            }
+                        }
+                    }
+                }
+            }
+        }
+    }
 }
 
 //restarta o jogo
